8e17d60c7e17ffe5cadc4067cbeb4104f0e68380
ennajohn <88430741+ennajohn@users.noreply.github.com>
2023-04-30 16:00:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+objects = [];
+status = "";
+song = "";
+
+function preload(){
+song = loadSound("beep-warning-6387.mp3");
+}
+
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+  }
+  
+  function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+  }
+
+  function draw() {
+    image(video, 0, 0, 380, 380);
+    
+        if(status != "")
+        {
+          r =  random(255);
+          g =  random(255);
+          b =  random(255);      
+          objectDetector.detect(video, gotResults);
+
+          for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Baby Detected";
+   
+            fill(r,g,b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+           
+            if(objects[i].label == "person")
+            {
+              document.getElementById("baby_detected").innerHTML = "Baby Found";
+              console.log("stop");
+              song.stop();
+            }
+            else
+            {
+              document.getElementById("baby_detected").innerHTML = "Baby Not Found";
+              console.log("play"); 
+              song.play();
+            }
+           }
+  
+          if(objects.length == 0)
+          {
+            document.getElementById("baby_detected").innerHTML = "Baby Not Found";
+            console.log("play"); 
+            song.play();
+          }
+        }
+  }
+  
+  function gotResults(error, results) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(results);
+    objects = results;
+  }
+
+
